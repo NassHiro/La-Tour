dfffc3c3122b79d6694330e70623650d0f038e43
NassHiro <64966400+NassHiro@users.noreply.github.com>
2020-05-11 19:02:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client(); 
 
-var prefix = 't!'
+var prefix = 'lt!'
 
 bot.on ('message',message => {
     if (message.content === prefix + 'message'){
